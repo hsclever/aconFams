c8e9cce8e3adf6e6852294660ec889af26402f6d
LAPTOP-D7ONCC55\hsjsc <lang.yun@acontech.co.kr>
2022-02-26 18:32:40 +0900

    init

---
+++ StudyProject/src/main/java/com/study/project/StudyProjectApplication.java
@@ -0,0 +1,14 @@
+package com.study.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("111");
+		SpringApplication.run(StudyProjectApplication.class, args);
+	}
+
+}
