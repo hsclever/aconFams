5888905f2a918afe324c8f088c21b66daf27f3ac
LAPTOP-D7ONCC55\hsjsc <lang.yun@acontech.co.kr>
2022-03-10 21:10:07 +0900

    화면만 띄운상태

--- lang.yun/StudyProject/src/main/java/com/study/project/StudyProjectApplication.java
+++ lang.yun/StudyProject/src/main/java/com/study/project/StudyProjectApplication.java
@@ -2,17 +2,15 @@ package com.study.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+/* @ComponentScan({"com.study.project"}) */
 public class StudyProjectApplication {
 	
 	public static void main(String[] args) {
 		System.out.println("111");
 		SpringApplication.run(StudyProjectApplication.class, args);
-	}
-	@PostMapping("/test")
-	public String test() {
-		return "home";
 	}
 }
