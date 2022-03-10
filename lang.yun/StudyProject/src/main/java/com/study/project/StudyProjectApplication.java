756009583b8d5d0734d167ecf11aeddf3df489f7
LAPTOP-D7ONCC55\hsjsc <lang.yun@acontech.co.kr>
2022-03-10 20:11:54 +0900

    # WARNING: head commit changed in the meantime
    12

--- lang.yun/StudyProject/src/main/java/com/study/project/StudyProjectApplication.java
+++ lang.yun/StudyProject/src/main/java/com/study/project/StudyProjectApplication.java
@@ -2,13 +2,17 @@ package com.study.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class StudyProjectApplication {
-
+	
 	public static void main(String[] args) {
 		System.out.println("111");
 		SpringApplication.run(StudyProjectApplication.class, args);
 	}
-
+	@PostMapping("/test")
+	public String test() {
+		return "home";
+	}
 }
--- lang.yun/StudyProject/src/main/webapp/WEB-INF/views/home.jsp
+++ lang.yun/StudyProject/src/main/webapp/WEB-INF/views/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	µÆ³ª
 </body>
 </html>
