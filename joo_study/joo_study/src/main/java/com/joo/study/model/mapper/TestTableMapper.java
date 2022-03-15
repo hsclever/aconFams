d0b39b8baa2d6558b1b6334ce6bafa9878d07af4
LAPTOP-D7ONCC55\hsjsc <lang.yun@acontech.co.kr>
2022-03-16 00:46:24 +0900

    1

--- joo_study/joo_study/src/main/java/com/joo/study/model/mapper/TestTableMapper.java
+++
@@ -1,9 +0,0 @@
-package com.joo.study.model.mapper;
-
-import java.util.List;
-import java.util.Map;
-
-public interface TestTableMapper {
-
-	public List<Map<String, Object>> SelectAllList() throws Exception;
-}
